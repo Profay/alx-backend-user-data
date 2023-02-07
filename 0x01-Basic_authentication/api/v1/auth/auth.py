0fc7f5f83ca4890a471830833df2e0500dec6477
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-07 11:09:49 +0000

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,14 @@ from typing import List, TypeVar
 class Auth:
     """ This class provides authentication"""
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str], strict_slashes=False) -> bool:
         """ Highlight the given path"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        elif path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ This function takes the header) """
