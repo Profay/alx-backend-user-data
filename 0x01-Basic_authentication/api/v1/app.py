7c91a30d5fbbb6d02d4793f6924a5a819dafdded
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-07 12:08:18 +0000

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -47,7 +47,7 @@ def before_request() -> str:
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
     ]
-    if auth.require_auth(path, excluded_paths):
+    if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
         abort(401)
