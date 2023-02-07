ca3b9cf941ca1173021b9c10127d2646c11a13db
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-07 10:37:02 +0000

    auths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,11 +2,21 @@
 """ This module contains auth functions"""
 
 
-from flask import requests
+from flask import request
+from typing import *
 
 
 class Auth:
     """ This class provides authentication"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Highlight the given path"""
-        return path in excluded_paths
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ This function takes the header) """
+        return request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ This function takes the current user """
+        return request
