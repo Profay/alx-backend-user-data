0a7fadb73134e13ea8243816352c8383d91387d5
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-09 19:45:55 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ This class provides basic authentication"""
+    def extract_base64_authorization_header(
+          self, authorization_header: str) -> str:
+        """ This function returns the authorization
+        header in Base64
+        """
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split('Basic ')[1]
