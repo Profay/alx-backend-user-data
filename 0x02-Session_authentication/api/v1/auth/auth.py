7080417c475cb5dcd99fc1d88e1afd4ab8644e85
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-09 21:12:42 +0000

    cp

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
-""" This module contains auth functions
+"""authentication template module
 """
+
 from flask import request
 from typing import List, Pattern, TypeVar
 
 
 class Auth:
-    """ This class provides authentication"""
-
+    """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Highlight the given path"""
-        if path is None or excluded_paths is None:
+        """requires authentication"""
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
-        elif path in excluded_paths or path + '/' in excluded_paths:
+        if path in excluded_paths or path + "/" in excluded_paths:
             return False
+
         for e_path in excluded_paths:
             if e_path.endswith('*'):
                 if path.startswith(i[:1]):
                     return False
-        else:
-            return True
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """ This function takes the header) """
+        """this function add authorization header"""
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ This function takes the current user """
+        """this method gets the current user"""
         None
