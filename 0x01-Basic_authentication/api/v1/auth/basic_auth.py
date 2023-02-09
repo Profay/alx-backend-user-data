7f34099c2ce9f296dd4e828ae8fd406c23cbe97c
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-09 19:59:24 +0000

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,3 +16,17 @@ class BasicAuth(Auth):
         elif not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split('Basic ')[1]
+
+    def decode_base64_authorization_header(
+          self, base64_authorization_header: str) -> str:
+        """ This function returns the decoded value of a
+        Base64 string base64_authorization_header: """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).\
+                          encode('utf-8')
+        except Exception:
+            return None
